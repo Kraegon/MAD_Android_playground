d044b0e7600c85dd9f1d822dc0ac25d4af5f23e9
Guus Beckett <guus@reupload.nl>
2015-10-27 13:01:46 +0100

    Added HueLight model

--- HueApp/app/src/main/java/comfycorp/hueapp/HueLight.java
+++ HueApp/app/src/main/java/comfycorp/hueapp/HueLight.java
@@ -6,5 +6,10 @@ package comfycorp.hueapp;
  * Created by Kraegon on 27/10/2015.
  */
 public class HueLight {
-
+    public int id;
+    public String name;
+    public Boolean isOn;
+    public int hue;
+    public int saturation;
+    public int brightness;
 }
