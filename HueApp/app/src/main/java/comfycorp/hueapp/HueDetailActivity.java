0b0def2278ce24fafbe9dc4f6a10e4bec27f2434
Guus Beckett <guus@reupload.nl>
2015-10-28 15:58:50 +0100

    Added detail page loading lamp data

--- HueApp/app/src/main/java/comfycorp/hueapp/HueDetailActivity.java
+++ HueApp/app/src/main/java/comfycorp/hueapp/HueDetailActivity.java
@@ -2,6 +2,9 @@ package comfycorp.hueapp;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.SurfaceView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  *
@@ -10,9 +13,40 @@ import android.support.v4.app.FragmentActivity;
  */
 public class HueDetailActivity  extends FragmentActivity {
 
+    private HueLight selectedHueLight = new HueLight();
+
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hue_details);
+
+        Bundle extras = getIntent().getExtras();
+
+        selectedHueLight.name = extras.getString("NAME");
+        selectedHueLight.id = extras.getInt("ID");
+        selectedHueLight.hue = extras.getInt("HUE");
+        selectedHueLight.saturation = extras.getInt("SATURATION");
+        selectedHueLight.brightness = extras.getInt("BRIGHTNESS");
+        selectedHueLight.isOn = extras.getBoolean("ISON");
+
+        updateView();
+    }
+
+    private void updateView() {
+
+        TextView labelName = (TextView) findViewById(R.id.textViewName);
+        TextView labelId = (TextView) findViewById(R.id.textViewId);
+        SeekBar seekBarHue = (SeekBar) findViewById(R.id.seekBarHue);
+        SeekBar seekBarSaturation = (SeekBar) findViewById(R.id.seekBarSat);
+        SeekBar seekBarBrightness = (SeekBar) findViewById(R.id.seekBarBright);
+        SurfaceView colorDisplay = (SurfaceView) findViewById(R.id.surfaceViewColor);
+
+        labelName.setText(selectedHueLight.name);
+        labelId.setText(Integer.toString(selectedHueLight.id));
+        seekBarHue.setProgress(selectedHueLight.hue);
+        seekBarSaturation.setProgress(selectedHueLight.saturation);
+        seekBarBrightness.setProgress(selectedHueLight.brightness);
+        colorDisplay.setBackgroundColor(selectedHueLight.GetColor());
+
     }
 }
--- HueApp/app/src/main/java/comfycorp/hueapp/MainActivity.java
+++ HueApp/app/src/main/java/comfycorp/hueapp/MainActivity.java
@@ -1,8 +1,6 @@
 package comfycorp.hueapp;
 
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +8,6 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
     BridgeMiddleMan bridgeMiddleMan = BridgeMiddleMan.getInstance();
@@ -22,15 +19,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
 
         bridgeMiddleMan.getAllLamps();
-
-        final Button button = (Button) findViewById(R.id.navTestButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent maliciousIntent = new Intent(getApplicationContext(), HueDetailActivity.class);
-                startActivity(maliciousIntent);
-            }
-        });
-
 
         final ListView huelistview = (ListView) findViewById(R.id.hueListView);
 
@@ -54,7 +42,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Intent i = new Intent(getApplicationContext(), HueDetailActivity.class);
-        System.out.println("onItemClick()");
+        HueLight hueLight = bridgeMiddleMan.lightArray.get(position);
+        i.putExtra("ID", hueLight.id);
+        i.putExtra("ISON", hueLight.isOn);
+        i.putExtra("NAME", hueLight.name);
+        i.putExtra("HUE", hueLight.hue);
+        i.putExtra("SATURATION", hueLight.saturation);
+        i.putExtra("BRIGHTNESS", hueLight.brightness);
         startActivity(i);
     }
 
